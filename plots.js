ad1d3d333a9e9d3de7513090d327dc4060da86d1
Colin Bartoe <colinbartoe@Colins-MacBook-Pro.local>
2021-05-23 16:12:24 -0400

    final update

--- plots.js
+++ plots.js
@@ -1,37 +1,40 @@
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-});
+function init() {
+  var selector = d3.select("#selDataset");
 
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+})}
 
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
-
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
+function optionChanged(newSample) {
+  console.log(newSample);
+  buildMetadata(newSample);
+  buildCharts(newSample);
 };
 
-// data
-var data = [trace1];
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
 
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
-  }
+    PANEL.html("");
+    PANEL.append("h6").text("ID: " + result.id);
+    PANEL.append("h6").text("Ethnicity: " + result.ethnicity);
+    PANEL.append("h6").text("Gender: " + result.gender);
+    PANEL.append("h6").text("Age: " + result.age);
+    PANEL.append("h6").text("Location: " + result.location);
+    PANEL.append("h6").text("BBType: " + result.bbtype);
+    PANEL.append("h6").text("WFreq: " + result.wfreq);
+  });
 };
 
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+
+init();
